623d06516004f829545802447a92efdcd21df802
Parth <parth.gupta@iauro.com>
2020-03-09 13:02:31 +0530

    parth: Heroku deployment

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/camera-filter'));
+app.use(express.static(__dirname + '/dist/camera-filter-pg'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/camera-filter/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/camera-filter-pg/index.html'));
 });
 
 // default Heroku port
